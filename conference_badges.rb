4264de6edd498f855f57f0b6c4c8cee58a0c81b0
Michael Harris <xero5k@yahoo.com>
2022-02-04 15:28:50 -0600

    completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator names
+  names.map do |name|
+    "Hello, my name is #{name}."
+  end
+end 
+
+
+def assign_rooms speakers
+  list = []
+  speakers.each_with_index do |speaker, index|
+    list <<  "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+  list
+end 
+
+def printer attendees
+  batch_badge_creator(attendees).each do |attendee|  
+    puts attendee
+  end 
+
+  assign_rooms(attendees).map do |attendee|
+    puts attendee
+  end 
+end 
+
+printer(["Johnny", "Sammy"]);
